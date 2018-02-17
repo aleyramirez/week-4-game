ae0297ab04b5a35ba089204bbc475e6c7035d50d
adan ley-ramirez <adanleyr@gmail.com>
2018-02-16 20:38:43 -0700

    Updating JavaScript file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,67 @@
 $(document).ready(function() {
 // Global Variables
-var yourScore = document.getElementById('your-score');
+var yourScore = document.getElementById('your-score'); //$('#your-score')
 var targetScore = document.getElementById('target-score');
-var randomNumber = getRandomNumber;
+var win = document.getElementById('wins');
+var loss = document.getElementById('losses');
+var redBtnVal = getRandomNumber(5);
+var blueBtnVal = getRandomNumber(10);
+var yellowBtnVal = getRandomNumber(15);
+var orangeBtnVal = getRandomNumber(20);
+yourScore.innerHTML = 0;
+loss.innerHTML = 0;
+win.innerHTML = 0;
 // Functions
 function getRandomNumber(a) {
-    return Math.floor(Math.random() * a);
+    return Math.floor (Math.random() * a);
 };
+
+function addScore(val) {
+    var scoreInt = parseInt(val);
+    var currentScore = parseInt(yourScore.innerHTML);
+    yourScore.innerHTML = scoreInt + currentScore;
+};
+
+function checkScore() {
+    var playerInt = parseInt(yourScore.innerHTML);
+    var targScore = parseInt(targetScore.innerHTML);
+    var wins = parseInt(win.innerHTML);
+    var losses = parseInt(loss.innerHTML);
+if (playerInt === targScore) {
+    wins++;
+    alert('You Win!');
+}else if (playerInt > targScore) {
+    losses++;
+    alert('You Lose!');
+};
+}
+
 
 // Event Handlers
 $('#red-button').on('click', function(){
-    var randomValue = function randomValue() {
-        var number = Math.floor(Math.random() * 5);
-        return number;
-    };
-    //var value = randomValue;
-    var addValues = randomValue + yourScore;
-    var currentValue = addValues;
-    yourScore.innerHTML = currentValue;
-    console.log(this.randomValue);
+    addScore(redBtnVal);
+    checkScore();
+    console.log(redBtnVal);
+});
+
+$('#blue-button').on('click', function(){
+    addScore(blueBtnVal);
+    checkScore();
+    console.log(blueBtnVal);
+});
+    
+$('#yellow-button').on('click', function(){
+    addScore(yellowBtnVal);
+    checkScore();
+    console.log(yellowBtnVal);
+});
+    
+$('#orange-button').on('click', function(){
+    addScore(orangeBtnVal);
+    checkScore();
+    console.log(orangeBtnVal);
 });
 
 targetScore.innerHTML = getRandomNumber(100);
 
-if (yourScore == targetScore) {
-    alert('Congrats, you win!');
-}
-// console.log();
 });
